73f00a96eb0dcc013b55a1b1f0f40496708af594
diegohenao819 <diegohenao819@gmail.com>
2021-10-31 10:57:04 -0500

    !

--- CLICK-PROF-REACT2/src/views/MiNavbar.jsx
+++ CLICK-PROF-REACT2/src/views/MiNavbar.jsx
@@ -27,7 +27,7 @@ function MiNavbar() {
         <div>
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-              <Navbar.Brand href="/">CLICK-PROF</Navbar.Brand>
+              <Navbar.Brand href="/">CLICK-PROF!</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
