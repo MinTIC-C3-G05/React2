e3ec5013d8c8fd271217c6546340b3423e9439ec
Johanes2903 <johan.zambrano2903@gmail.com>
2021-11-14 11:46:39 -0500

    Videos aletatorios en Carrusel y Cards

--- CLICK-PROF-REACT2/src/components/TarjetasVideos.jsx
+++ CLICK-PROF-REACT2/src/components/TarjetasVideos.jsx
@@ -2,6 +2,39 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 
+let videos = ['https://www.youtube.com/embed/jaxgeXPgAz0',
+ 'https://www.youtube.com/embed/lX6JcybgDFo', 
+ 'https://www.youtube.com/embed/D6_qpaSxAQc', 
+ 'https://www.youtube.com/embed/5v-wyR5emRw',
+ 'https://www.youtube.com/embed/saTZ6TRS_HE', 
+ 'https://www.youtube.com/embed/Y486eiRm1q8', 
+ 'https://www.youtube.com/embed/ztrtZ0en8dw',
+ 'https://www.youtube.com/embed/iYwI3hzaXTM', 
+ 'https://www.youtube.com/embed/f_Febo0yKhc', 
+ 'https://www.youtube.com/embed/Foy3PBmQKks',
+ 'https://www.youtube.com/embed/iGlGc057CLk', 
+ 'https://www.youtube.com/embed/MfsfseVRZOQ', 
+ 'https://www.youtube.com/embed/Q6MiwSun4L4',
+ 'https://www.youtube.com/embed/RqLd5O5yBLQ', 
+ 'https://www.youtube.com/embed/xiohfyQsZxc', 
+ 'https://www.youtube.com/embed/ag3RnEaB3zM',
+ 'https://www.youtube.com/embed/R1vskiVDwl4', 
+ 'https://www.youtube.com/embed/-vZXgApsPCQ', 
+ 'https://www.youtube.com/embed/IZRzkAw-Fz8',
+ 'https://www.youtube.com/embed/Wo-C-jgA4Y8', 
+ 'https://www.youtube.com/embed/R3KeD06ti6c', 
+ 'https://www.youtube.com/embed/yoa0eIuWcsg'];
+
+ 
+ function numRandom(){
+
+  let num = Math.floor(Math.random()*videos.length);
+  return num;
+
+}
+
+let CardNum = numRandom();
+
 
 const TarjetasVideos = () => {
     return (
@@ -10,7 +43,7 @@ const TarjetasVideos = () => {
           <Card>
         
             <Card.Body>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/jaxgeXPgAz0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src={videos[CardNum]} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </Card.Body>
             <Card.Footer>
               <small className="text-muted">Last updated 3 mins ago</small>
@@ -18,7 +51,7 @@ const TarjetasVideos = () => {
           </Card>
           <Card>
             <Card.Body>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/D6_qpaSxAQc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src={videos[CardNum + 1]}  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </Card.Body>
             <Card.Footer>
               <small className="text-muted">Last updated 3 mins ago</small>
@@ -26,7 +59,7 @@ const TarjetasVideos = () => {
           </Card>
           <Card>
             <Card.Body>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/5v-wyR5emRw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src={videos[CardNum - 1]}  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </Card.Body>
             <Card.Footer>
               <small className="text-muted">Last updated 3 mins ago</small>
