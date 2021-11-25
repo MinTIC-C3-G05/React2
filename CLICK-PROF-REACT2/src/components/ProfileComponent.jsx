73e2ef58994a7e918686883f7ec8c13089fad896
diegohenao819 <diegohenao819@gmail.com>
2021-11-24 20:43:50 -0500

    level

--- CLICK-PROF-REACT2/src/components/ProfileComponent.jsx
+++ CLICK-PROF-REACT2/src/components/ProfileComponent.jsx
@@ -1,17 +1,22 @@
 import React, {useState} from 'react'
 import {useAuth0} from '@auth0/auth0-react'
 import axios from 'axios'
+import {
+    
+    Link
+  } from "react-router-dom";
 
 function ProfileComponent() {
     const {user, isAuthenticated, isLoading} = useAuth0()
 
 
-    const [level, setLevel] = useState("TAKE OUR TEST")
+    const [level, setLevel] = useState("")
 
     axios.get("/test").then((response)=>{
         response.data.map(element=>{
             if(user.email === element.email){
                 setLevel(element.level)
+                console.log(element)
 
             }
         })
@@ -28,7 +33,7 @@ function ProfileComponent() {
                     <img src={user.picture} alt={user.name}/>
                     <h2>Hello:{user.name}</h2>
                     <p>Email: {user.email}</p>
-                    <p>Level:{level}</p>
+                    <p>Level:{level===""? <Link to="/quiz">Please take our test</Link>: level}</p>
                 </div>
             )
     )
