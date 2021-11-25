ae95946a5efbf879d52c34a2e7f2fdb86d4df262
diegohenao819 <diegohenao819@gmail.com>
2021-11-24 20:16:47 -0500

    nivel en perfil

--- CLICK-PROF-REACT2/src/components/ProfileComponent.jsx
+++ CLICK-PROF-REACT2/src/components/ProfileComponent.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useAuth0} from '@auth0/auth0-react'
+import axios from 'axios'
 
 function ProfileComponent() {
     const {user, isAuthenticated, isLoading} = useAuth0()
+
+
+    const [level, setLevel] = useState("TAKE OUR TEST")
+
+    axios.get("/test").then((response)=>{
+        response.data.map(element=>{
+            if(user.email === element.email){
+                setLevel(element.level)
+
+            }
+        })
+    })
+
 
 
     if (isLoading){
@@ -14,6 +28,7 @@ function ProfileComponent() {
                     <img src={user.picture} alt={user.name}/>
                     <h2>Hello:{user.name}</h2>
                     <p>Email: {user.email}</p>
+                    <p>Level:{level}</p>
                 </div>
             )
     )
