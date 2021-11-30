e4b684ebd77be04a5e31cc6d395a4fb483c884f9
dannyacv1996 <dannyacv10@gmail.com>
2021-11-30 01:31:07 -0500

    Update VideosPage.jsx
    se agrega el nivel en vídeos

--- CLICK-PROF-REACT2/src/views/VideosPage.jsx
+++ CLICK-PROF-REACT2/src/views/VideosPage.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
 import ControlledCarousel from "../components/ControlledCarousel";
 import TarjetasVideos from '../components/TarjetasVideos';
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 function VideosPage() {
+
+
+    const { user } = useAuth0();
+
+    const [level, setLevel] = useState("");
+
+    axios.get("/test").then((response) => {
+        response.data.map((element) => {
+            if (typeof(user) !== 'undefined') {
+                if (user.email === element.email) {
+                    setLevel(element.level);
+                }
+            }
+        });
+    });
+
+
+
     return (
         <React.Fragment>
 
             <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-            <h3 className="font-family" >VIDEOS</h3>
+            <h3 className="font-family" >VIDEOS {level === "" ? "" : level}</h3>
             <br />
             <br />
             <br />
@@ -25,4 +45,3 @@ function VideosPage() {
 }
 
 export default VideosPage
-
